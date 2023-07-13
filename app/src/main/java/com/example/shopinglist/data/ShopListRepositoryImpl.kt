75c45e46cec6ac2ce0b5f38711e414ce654188c4
Alex <mgood957@gmail.com>
2023-07-13 21:59:18 +0300

    line layuot gone

--- app/src/main/java/com/example/shopinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shopinglist/data/ShopListRepositoryImpl.kt
@@ -9,19 +9,19 @@ object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopListLD = MutableLiveData<List<ShopItem>>()
 
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id) })
 
     private var autoIncrementId: Int = 0
 
     init {
-        for (i in 0 until 10){
+        for (i in 0 until 10) {
             val item = ShopItem("Name $i", i, true)
             addShopItem(item)
         }
     }
 
     override fun addShopItem(shopItem: ShopItem) {
-        if (shopItem.id == ShopItem.UNDEFINED_ID){
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
             shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
@@ -49,7 +49,7 @@ object ShopListRepositoryImpl : ShopListRepository {
         return shopListLD
     }
 
-    private fun updateList(){
+    private fun updateList() {
         shopListLD.value = shopList.toList()
     }
 
